886aab7137a21b525859aa4a243d490bf34283d8
Jordan Hamill <jordan@moneykit.com>
2023-05-02 11:54:47 +0100

    Update moneykit sdk

--- create_link/backend_python/app/api.py
+++ create_link/backend_python/app/api.py
@@ -5,6 +5,8 @@ import moneykit
 import moneykit.models
 import pydantic
 from fastapi import APIRouter, Body, status
+from moneykit.api.access_token import generate_access_token
+from moneykit.api.link_session import create_link_session, exchange_token
 
 from app.settings import get_settings
 
@@ -12,22 +14,25 @@ router = APIRouter(prefix="/linking")
 
 
 @functools.lru_cache
-def create_moneykit_client() -> moneykit.ApiClient:
+def create_moneykit_client() -> moneykit.AuthenticatedClient:
     settings = get_settings()
     # Generate a bearer access token that is valid for 60 minutes.
     # This client should be cached to avoid regenerating a token on every request.
     # The bearer token should be regenerated before expiry or after a `api_error.auth.expired_access_token` error code
     # is returned by a request.
-    unauthenticated_client = moneykit.ApiClient(configuration=moneykit.Configuration(host=settings.moneykit_url))
-    response = moneykit.AccessTokenApi(unauthenticated_client).generate_access_token(
-        grant_type="client_credentials",
-        client_id=settings.moneykit_client_id,
-        client_secret=settings.moneykit_client_secret.get_secret_value(),
+
+    unauthenticated_client = moneykit.Client(base_url=settings.moneykit_url)
+
+    response = generate_access_token.sync(
+        client=unauthenticated_client,
+        form_data=moneykit.models.BodyGenerateAccessTokenAuthTokenPost(
+            grant_type="client_credentials",
+            client_id=settings.moneykit_client_id,
+            client_secret=settings.moneykit_client_secret.get_secret_value(),
+        ),
     )
 
-    config = moneykit.Configuration(host=settings.moneykit_url)
-    config.access_token = response.access_token  # Bug with constructor overwriting to `None`
-    client = moneykit.ApiClient(configuration=config)
+    client = moneykit.AuthenticatedClient(base_url=settings.moneykit_url, token=response.access_token)
     return client
 
 
@@ -47,12 +52,12 @@ class ExchangeTokenForLinkResponse(pydantic.BaseModel):
 @router.post("/session", status_code=status.HTTP_201_CREATED, response_model=NewLinkSessionResponse)
 async def new_link_session() -> NewLinkSessionResponse:
     client = create_moneykit_client()
-    link_session_api = moneykit.LinkSessionApi(client)
-    response = link_session_api.create_link_session(
-        moneykit.models.CreateLinkSessionBody(
-            customer_user=moneykit.models.CustomerUser(id="examples-create_link-test-user"),
+    response = create_link_session.sync(
+        client=client,
+        json_body=moneykit.models.CreateLinkSessionRequest(
+            customer_user=moneykit.models.LinkSessionCustomerUser(id="examples-create_link-test-user"),
             link_tags=["examples:create_link"],
-            settings=moneykit.models.SettingOverrides(
+            settings=moneykit.models.LinkSessionSettingOverrides(
                 link_permissions=moneykit.models.LinkPermissions(
                     requested=[
                         moneykit.models.RequestedLinkPermission(
@@ -77,24 +82,23 @@ async def new_link_session() -> NewLinkSessionResponse:
                         ),
                     ]
                 ),
-                # TODO: Fix API spec naming
-                products=moneykit.models.Products1(
-                    account_numbers=moneykit.models.AccountNumbers(
+                products=moneykit.models.ProductsSettings(
+                    account_numbers=moneykit.models.AccountNumbersProductSettings(
                         required=False,
                         prefetch=True,
                     ),
-                    identity=moneykit.models.Identity(
+                    identity=moneykit.models.IdentityProductSettings(
                         required=False,
                         prefetch=True,
                     ),
-                    transactions=moneykit.models.Transactions(
+                    transactions=moneykit.models.TransactionsProductSettings(
                         required=False,
                         prefetch=True,
                         extend_history=False,
                     ),
                 ),
             ),
-        )
+        ),
     )
     return NewLinkSessionResponse(link_session_token=response.link_session_token)
 
@@ -104,9 +108,8 @@ async def exchange_token_for_link(
     body: Annotated[ExchangeTokenForLinkRequest, Body()],
 ) -> ExchangeTokenForLinkResponse:
     client = create_moneykit_client()
-    link_session_api = moneykit.LinkSessionApi(client)
-    response = link_session_api.exchange_token(
-        moneykit.models.ExchangeTokenBody(exchangeable_token=body.exchangeable_token)
+    response = exchange_token.sync(
+        client=client, json_body=moneykit.models.ExchangeTokenRequest(exchangeable_token=body.exchangeable_token)
     )
     print(f"MoneyKit link id: {response.link_id}")
     return ExchangeTokenForLinkResponse(
