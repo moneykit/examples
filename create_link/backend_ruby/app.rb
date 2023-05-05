0d449711d23c86005f1ddcef4079eaddb39a83f3
Jordan Hamill <jordan@moneykit.com>
2023-05-05 10:30:18 +0100

    Simplify ruby example

--- create_link/backend_ruby/app.rb
+++ create_link/backend_ruby/app.rb
@@ -1,5 +1,7 @@
-require "sinatra"
-require "moneykit"
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'moneykit'
 require 'debug'
 
 set :port, 8000
@@ -13,75 +15,69 @@ def create_moneykit_client
   # is returned by a request.
   configuration = MoneyKit::Configuration.new
   configuration.server_index = nil
-  configuration.host = ENV["MONEYKIT_URL"]
+  configuration.host = ENV['MONEYKIT_URL']
   unauthenticated_client = MoneyKit::ApiClient.new(configuration)
 
   access_token_client = MoneyKit::AccessTokenApi.new(unauthenticated_client)
   response =
     access_token_client.generate_access_token(
       {
-        grant_type: "client_credentials",
-        client_id: ENV["MONEYKIT_CLIENT_ID"],
-        client_secret: ENV["MONEYKIT_CLIENT_SECRET"]
+        grant_type: 'client_credentials',
+        client_id: ENV['MONEYKIT_CLIENT_ID'],
+        client_secret: ENV['MONEYKIT_CLIENT_SECRET']
       }
     )
 
   configuration.access_token = response.access_token
-  api_client = MoneyKit::ApiClient.new(configuration)
-  api_client
+  MoneyKit::ApiClient.new(configuration)
 end
 
 def moneykit_client
-  @mk_client ||= begin
-    create_moneykit_client
-  end
+  @moneykit_client ||= create_moneykit_client
 end
 
-get "/health-check" do
-  { project: "create_link/backend_ruby" }.to_json
+get '/health-check' do
+  { project: 'create_link/backend_ruby' }.to_json
 end
 
-post "/linking/session" do
+post '/linking/session' do
   link_session_api = MoneyKit::LinkSessionApi.new(moneykit_client)
   response = link_session_api.create_link_session(
-    MoneyKit::CreateLinkSessionRequest.new({
-      customer_user: MoneyKit::CustomerUser.new({id: "examples-create_link-test-user"}),
-      link_tags: ["examples:create_link"],
-      settings: MoneyKit::LinkSessionSettingOverrides.new({
-        link_permissions: MoneyKit::LinkPermissions.new({
+    {
+      customer_user: { id: 'examples-create_link-test-user' },
+      link_tags: ['examples:create_link'],
+      settings: {
+        link_permissions: {
           requested: [
-            MoneyKit::RequestedLinkPermission.new({scope: "accounts", reason: "play with MoneyKit examples.", required: true}),
-            MoneyKit::RequestedLinkPermission.new({scope: "account_numbers", reason: "play with MoneyKit examples.", required: true}),
-            MoneyKit::RequestedLinkPermission.new({scope: "identity", reason: "play with MoneyKit examples.", required: true}),
-            MoneyKit::RequestedLinkPermission.new({scope: "transactions", reason: "play with MoneyKit examples.", required: true}),
+            { scope: 'accounts', reason: 'play with MoneyKit examples.', required: true },
+            { scope: 'account_numbers', reason: 'play with MoneyKit examples.', required: true },
+            { scope: 'identity', reason: 'play with MoneyKit examples.', required: true },
+            { scope: 'transactions', reason: 'play with MoneyKit examples.', required: true }
 
           ]
-        }),
-        products: MoneyKit::ProductsSettings.new({
-          account_numbers: MoneyKit::AccountNumbersProductSettings.new({
+        },
+        products: {
+          account_numbers: {
             required: false,
-            prefetch: true,
-          })
+            prefetch: true
+          }
 
-        }),
-
-      })
-    })
+        }
+      }
+    }
   )
 
   status 201
   { link_session_token: response.link_session_token }.to_json
 end
 
-post "/linking/exchange-token" do
+post '/linking/exchange-token' do
   request.body.rewind
   data = JSON.parse request.body.read
-  exchangeable_token = data["exchangeable_token"]
+  exchangeable_token = data['exchangeable_token']
 
   link_session_api = MoneyKit::LinkSessionApi.new(moneykit_client)
-  response = link_session_api.exchange_token(
-    MoneyKit::ExchangeTokenRequest.new({exchangeable_token: exchangeable_token})
-  )
+  response = link_session_api.exchange_token({ exchangeable_token: exchangeable_token })
 
   status 202
   { moneykit_link_id: response.link_id, institution_name: response.link.institution_name }.to_json
